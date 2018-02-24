d591ca434a4218d189260abfc8cebd205524a665
imaresss <pulkitkedia1197@gmail.com>
2018-02-24 20:06:01 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -27,7 +27,7 @@ class NeuralNetwork():
     # We train the neural network through a process of trial and error.
     # Adjusting the synaptic weights each time.
     def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-        for iteration in xrange(number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
             # Pass the training set through our neural network (a single neuron).
             output = self.think(training_set_inputs)
 
